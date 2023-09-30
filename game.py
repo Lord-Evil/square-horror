2904b524aea168485cf75e26fbe63e05405f8136
smolovk <smolov326@gmail.com>
2023-09-30 22:25:28 +0300

    add cell based movement

--- game.py
+++ game.py
@@ -5,28 +5,60 @@ from pygame.locals import *
 class Cubert(pygame.sprite.Sprite):
     def __init__(self, initial_position, cubert_size):
         pygame.sprite.Sprite.__init__(self)
-        # self.image = pygame.Surface([25, 25])  # Cubert's size. Adjust as needed.
-        # self.image.fill((255, 0, 0))  # A deliciously devilish shade of red.
         self.health = 3
         self.size = cubert_size
 
-        self.image = pygame.image.load(os.path.join(".", "art", "cubert.png")).convert()
+        unscaled_image = pygame.image.load(os.path.join(".", "art", "cubert.png")).convert()
+        self.image = pygame.transform.scale(unscaled_image, self.size)
         self.rect = self.image.get_rect()
         self.rect.topleft = initial_position  # This sets the initial position.
         self.is_circle = False
 
+    def calc_cell_coords(self):
+        room_coords = [
+            (self.rect.topleft[0]-(screen_size[0]-room_size)/2),
+            (self.rect.topleft[1]-(screen_size[1]-room_size)/2)
+        ]
+        return (room_coords[0]/(room_size/10)+1, room_coords[1]/(room_size/10)+1)
+
+    def check_cell(self, cell):
+        print(int(cell[0]))
+        if (cell[0] > len(cells) or cell[0]<=0):
+            return False
+        if (cell[1] > len(cells[int(cell[0])-1]) or cell[1]<=0):
+            return False
+        # print(len(cells), len(cells[0]))
+        # print(cell[1], len(cells[int(cell[0])]))
+        # print(cell[0], len(cells))
+        return True
+
+    def move_to(self, dir):
+        if dir=="right":
+            if(self.check_cell((self.calc_cell_coords()[0]+1, self.calc_cell_coords()[1]))):
+                self.rect.move_ip(cell_size, 0)
+        if dir=="left":
+            if(self.check_cell((self.calc_cell_coords()[0]-1, self.calc_cell_coords()[1]))):
+                self.rect.move_ip(-cell_size, 0)
+        if dir=="up":
+            if(self.check_cell((self.calc_cell_coords()[0], self.calc_cell_coords()[1]-1))):
+                self.rect.move_ip(0, -cell_size)
+        if dir=="down":
+            if(self.check_cell((self.calc_cell_coords()[0], self.calc_cell_coords()[1]+1))):
+                self.rect.move_ip(0, cell_size)
+
+
     def update(self, event):
         keys = pygame.key.get_pressed()
         
-        if event.type == KEYDOWN:
-            if keys[pygame.K_RIGHT]:
-                self.rect.move_ip(5, 0)
-            if keys[pygame.K_LEFT]:
-                self.rect.move_ip(-5, 0)
-            if keys[pygame.K_UP]:
-                self.rect.move_ip(0, -5)
-            if keys[pygame.K_DOWN]:
-                self.rect.move_ip(0, 5)
+        if event.type == KEYUP:
+            if event.key ==K_RIGHT:
+                self.move_to("right")
+            if event.key ==K_LEFT:
+                self.move_to("left")
+            if event.key ==K_UP:
+                self.move_to("up")
+            if event.key ==K_DOWN:
+                self.move_to("down")
             if keys[pygame.K_SPACE]:
                 self.is_circle = True
                 self.image = pygame.image.load(os.path.join(".", "art", "cubert-circle.png")).convert()
@@ -41,7 +73,9 @@ white = (255,255,255)
 black = (0,0,0)
 red = (255, 0, 0)
 
-cubert_size = [30, 30]
+
+cubert_size = [50, 50]
+cell_size = cubert_size[0]
 
 obstacle_img = pygame.image.load(os.path.join(".", "art", "obstacle.png"))
 obstacles = [
@@ -53,6 +87,16 @@ wall_img = pygame.image.load(os.path.join(".", "art", "wall.png"))
 
 room_size = 500
 
+cells = []
+
+for i in range(10):
+    cells.append([])
+    for j in range(10):
+        cells[i].append("none")
+
+screen_size = (800, 600)
+
+
 def game_main():
     pygame.init()
     pygame.mixer.init()
@@ -61,11 +105,10 @@ def game_main():
     # sound = pygame.mixer.Sound(os.path.join(".", "sounds", "game.ogg"))
     # sound.play(loops=5)
 
-    screen_size = (800, 600)
     screen = pygame.display.set_mode(screen_size)
     pygame.key.set_repeat(35)
 
-    cubert = Cubert([screen_size[0]/2-(cubert_size[0]/2), screen_size[1]/2-(cubert_size[1]/2)+screen_size[1]/3], cubert_size=cubert_size)  # Initialize Cubert.
+    cubert = Cubert([screen_size[0]-room_size+150, screen_size[1]-room_size+500/10*3], cubert_size=cubert_size)  # Initialize Cubert.
 
     running = True
     while running:
