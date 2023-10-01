87b64310adb49d2195d75c59efa7824336d4eb1f
Lord_Evil <Lord_Evil@om-webmasters.org>
2023-10-01 21:04:02 +1000

    sounds

--- game.py
+++ game.py
@@ -3,13 +3,19 @@ import pygame
 import os
 from pygame.locals import *
 
+pygame.init()
+pygame.mixer.init()
+
 obstacle_img = pygame.image.load(os.path.join(".", "art", "obstacle.png"))
 coin_img = pygame.image.load(os.path.join(".", "art", "coin.png"))
-coin_img = pygame.transform.scale(coin_img, (30, 30))
+#coin_img = pygame.transform.scale(coin_img, (30, 30))
 floor_img = pygame.image.load(os.path.join(".", "art", "floor.png"))
 wall_img = pygame.image.load(os.path.join(".", "art", "wall.png"))
 bonusJ_img = pygame.image.load(os.path.join(".", "art", "jump_bonus.png"))
 bonusS_img = pygame.image.load(os.path.join(".", "art", "speed_bonus.png"))
+
+death_snd = pygame.mixer.Sound(os.path.join(".", "sounds", "death.wav"))
+coin_snd = pygame.mixer.Sound(os.path.join(".", "sounds", "coin.wav"))
 
 cell_size = 50
 
@@ -18,11 +24,30 @@ boardYOffset = 50
 
 screen_size = (800, 600)
 
+class CoinCounter():
+    def __init__(self, screen):
+        self.screen = screen
+        self.counter = 0
+        self.font = pygame.font.Font('kongtext.ttf', 24)
+        self.text = self.font.render('00:00', True, "#222034", "#cbdbfc")
+        self.textRect = self.text.get_rect()
+        self.textRect.center = (100, 55)
+
+    def draw(self):
+        rect = pygame.Rect(0, 30, cell_size, cell_size)
+        self.screen.blit(coin_img, rect.topleft)
+        self.text = self.font.render(str(self.counter), True, "#222034", "#cbdbfc")
+        self.screen.blit(self.text, self.textRect)
+
+    def addOne(self):
+        self.counter += 1
+
 class Level():
     cubert = None
     cells = None
     def __init__(self, screen, level="01"):
         self.screen = screen
+        self.coinCounter = CoinCounter(screen)
         self.loadMap(level)
 
     def loadMap(self, mapName):
@@ -72,10 +97,7 @@ class Level():
         self.screen.blit(obstacle_img, rect.topleft)
 
     def putCoin(self, x, y):
-        (xCord, yCord) = self.getCoords(x, y)
-        xCord += 10
-        yCord += 10
-        rect = pygame.Rect(xCord, yCord, cell_size-20, cell_size-20)
+        rect = pygame.Rect(*self.getCoords(x, y), cell_size, cell_size)
         self.screen.blit(coin_img, rect.topleft)
 
     def putBonus(self, bonusType, x, y):
@@ -98,6 +120,7 @@ class Level():
                 self.screen.blit(floor_img, pygame.Rect(x, y, cell_size, cell_size))
         pygame.draw.rect(self.screen, ("#ffffff"), ((800 - self.room_size)/2, (600 - self.room_size)/2, self.room_size, self.room_size), 3)
 
+        self.coinCounter.draw()
         self.redraw()
 
     def canMove(self, x, y):
@@ -117,6 +140,8 @@ class Level():
 
     def eatCoin(self, x, y):
         self.cells[y][x] = 'O'
+        self.coinCounter.addOne()
+        coin_snd.play()
 
     def isBonus(self, x, y):
         return self.cells[y][x] in ["S", "J"]
@@ -248,8 +273,13 @@ class Timer():
         self.running = True
         self.startTime=pygame.time.get_ticks()
 
-    def stop(self):
+    def stop(self, win=False):
         self.running = False
+        pygame.mixer.music.stop()
+        if win:
+            pass
+        else:
+            channel = death_snd.play()
 
     def draw(self):
         timePast = int((pygame.time.get_ticks() - self.startTime)/1000)
@@ -263,8 +293,6 @@ class Timer():
         self.screen.blit(self.text, self.textRect)
 
 def game_main(music=True):
-    pygame.init()
-    pygame.mixer.init()
     pygame.display.set_caption('Horror Cube')
     icon = pygame.image.load('art/cubert-speed-circle.png')
     pygame.display.set_icon(icon)
@@ -272,14 +300,14 @@ def game_main(music=True):
     sound = pygame.mixer.music.load(os.path.join(".", "sounds", "game.ogg"))
     #sound = pygame.mixer.Sound(os.path.join(".", "sounds", "game.ogg"))
     if music:
-        pygame.mixer.music.play(loops=-1)
+        pygame.mixer.music.play(-1)
 
     screen = pygame.display.set_mode(screen_size)
     pygame.key.set_repeat(200)
 
     level = Level(screen, "01")
     cubert = level.getCubert()
-    timer = Timer(screen, 30)
+    timer = Timer(screen, 10)
     timer.start()
     if not cubert:
         print("Cubert is missing!")
